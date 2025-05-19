509c0dcf9b7a0f8bc1c4af38adb9ffebcfc16a82
Cameron Howard <chow419@wgu.edu>
2025-05-19 17:32:27 +0000

    Project Created.

---
+++ D424 - Software Engineering Capstone/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace D424___Software_Engineering_Capstone
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ D424 - Software Engineering Capstone/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace D424___Software_Engineering_Capstone;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
